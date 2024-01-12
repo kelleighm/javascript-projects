bb13326bad8461bfe5a9980da9c682a6cd0e7202
kelleighm <kelleighmmartin@gmail.com>
2024-01-11 21:36:12 -0600

    Class 2 Studio

--- booleans-and-conditionals/studio/data-variables-conditionals.js
+++ booleans-and-conditionals/studio/data-variables-conditionals.js
@@ -14,23 +14,22 @@ let minimumFuelTemp = -300;
 let maximumFuelTemp= -150;
 let fuelLevel = 100;
 let weatherStatus = ("clear");
-let preparedForLiftOff = true 
+let preparedForLiftOff = true; 
 
 // add logic below to verify total number of astronauts for shuttle launch does not exceed 7
-if (astronautCount <= 7) {}
+if (astronautCount <= 7) preparedForLiftOff = false;
 // add logic below to verify all astronauts are ready
-if (astronautStatus === "ready") {}
+if (astronautStatus !== "ready") preparedForLiftOff = false; 
 // add logic below to verify the total mass does not exceed the maximum limit of 850000
-if (totalMassKg < maximumMassLimit) {}
+if (totalMassKg >= maximumMassLimit) preparedForLiftOff = false;
 // add logic below to verify the fuel temperature is within the appropriate range of -150 and -300
-if (fuelTempCelsius < -300 || fuelTempCelsius >= -150) {}
+if(fuelTempCelsius < minimumFuelTemp || fuelTempCelsius > maximumFuelTemp) preparedForLiftOff = false;
 // add logic below to verify the fuel level is at 100%
-if (fuelLevel === 100) {}
+if (fuelLevel !== 100) preparedForLiftOff = false;
 // add logic below to verify the weather status is clear
-if (weatherStatus === "clear") {}
+if (weatherStatus !== "clear") preparedForLiftOff = false;
 // Verify shuttle launch can proceed based on above conditions
-if (preparedForLiftOff == true) {
-    console.log("All systems are a go! Initiating space shuttle launch sequence."); }
+console.log("All systems are a go! Initiating space shuttle launch sequence.");
 console.log("--------------------------------------------------------------------");
 console.log("Date: " + date);
 console.log("Time: " + time);
@@ -42,4 +41,8 @@ console.log("Total Mass: " + totalMassKg + " kg");
 console.log("Fuel Temperature: " + fuelTempCelsius + " C");
 console.log("Weather Status: " + weatherStatus);
 console.log("--------------------------------------------------------------------");
-console.log("Have a safe trip astronauts!");
+if(preparedForLiftOff === false){
+    console.log("Conditions for launch do not meet requirements. Mission aborted!");
+} else {
+    console.log("Have a safe trip astronauts!");
+}
