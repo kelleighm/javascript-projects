a17f6f406aeb631f6cb9a1d036ac5239b7f41dc3
kelleighm <kelleighmmartin@gmail.com>
2024-02-12 16:23:38 -0600

    Exceptions Exercise

--- exceptions/exercises/test-student-labs.js
+++ exceptions/exercises/test-student-labs.js
@@ -2,7 +2,12 @@ function gradeLabs(labs) {
   for (let i=0; i < labs.length; i++) {
     let lab = labs[i];
     let result = lab.runLab(3);
-    console.log(`${lab.student} code worked: ${result === 27}`);
+    console.log(`${lab.student} code worked: ${result === 27}`)
+    try {
+      runLab();
+    } catch(TypeError) {
+    console.log("Error thrown!");
+    }
   }
 }
 
@@ -22,3 +27,26 @@ let studentLabs = [
 ];
 
 gradeLabs(studentLabs);
+
+let studentLabs2 = [
+  {
+      student: 'Blake',
+      myCode: function (num) {
+        return Math.pow(num, num);
+      }
+  },
+  {
+      student: 'Jessica',
+      runLab: function (num) {
+        return Math.pow(num, num);
+      }
+  },
+  {
+      student: 'Mya',
+      runLab: function (num) {
+        return num * num;
+      }
+  }
+];
+
+gradeLabs(studentLabs2);
