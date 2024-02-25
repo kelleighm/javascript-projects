f42b11337d1160fdbedc795cd04cd6dfbb8411ae
kelleighm <kelleighmmartin@gmail.com>
2024-02-25 12:40:46 -0600

    DOM and Events Exercise

--- dom-and-events/exercises/script.js
+++ dom-and-events/exercises/script.js
@@ -3,8 +3,25 @@ function init () {
     const button = document.getElementById("liftoffButton");
     const paragraph = document.getElementById("statusReport");
 
-    // Put your code for the exercises here.
-    
+    button.addEventListener('click', event => {
+        paragraph.innerHTML = 'Houston! We have liftoff!';
+        });
+
+
+    missionAbort.addEventListener("mouseover", function( event ) {
+        event.target.style.backgroundColor = "red";
+        });
+
+    missionAbort.addEventListener("mouseout", function( event ) {
+        event.target.style.backgroundColor = "";
+        });
+
+    abortMission.addEventListener('click', function ( event ) {
+        let answer = window.confirm("Are you sure you want to abort the mission?");
+            if (answer == true) {
+                window.alert("Mission aborted! Space shuttle returning home");
+            }
+    });
 }
 
 window.addEventListener("load", init);
