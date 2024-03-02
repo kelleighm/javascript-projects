464041dec7ba14a473798742faa575001934f289
kelleighm <kelleighmmartin@gmail.com>
2024-03-02 11:57:05 -0600

    Fetch Exercise

--- functions/try-it/Practicing.js
+++ functions/try-it/Practicing.js
@@ -1,18 +1,48 @@
+const input = require("readline-sync");
+
+const oldPointStructure = {
+  1: ['A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T'],
+  2: ['D', 'G'],
+  3: ['B', 'C', 'M', 'P'],
+  4: ['F', 'H', 'V', 'W', 'Y'],
+  5: ['K'],
+  8: ['J', 'X'],
+  10: ['Q', 'Z']
+};
+
+let newPointStructure = transform(oldPointStructure);
 
 
 
-function vowelBonusScorer(word) {
-let score = 0;
-word = word.toLowerCase();
-let vowels = ["a", "e", "i", "o", "u"]; 
-for (let i = 0; i < word.length; i++) {
-  if (vowels.includes(word[i])) {
-    score = score + 3;
-  } else { 
-    score = score + 1;
-  }
-}
-return console.log(score);
-}
+function oldScrabbleScorer(word) {
+	word = word.toUpperCase();
+	let letterPoints = "";
+ 
+	for (let i = 0; i < word.length; i++) {
+ 
+	  for (const pointValue in oldPointStructure) {
+ 
+		 if (oldPointStructure[pointValue].includes(word[i])) {
+			letterPoints += `Points for '${word[i]}': ${pointValue}\n`
+		 }
+ 
+	  }
+	}
+	return letterPoints;
+ };
 
-vowelBonusScorer("ATE")
+
+
+
+function transform() {
+   newTransform = { };
+   for (key in oldPointStructure) {
+      keyArray = oldPointStructure[key];
+      for (i = 0; i < keyArray.length; i++) {
+         newTransform[keyArray[i].toLowerCase]
+      }
+   }
+   return newTransform; 
+};
+
+
--- unit-testing/studio/index.js
+++ unit-testing/studio/index.js
@@ -1,7 +1,26 @@
 
 let launchcode = {
-  
-}
+    organization: 'nonprofit',
+    executiveDirector: 'Jeff', 
+    percentageCoolEmployees: 100,
+    programsOffered: ["Web Development", "Data Analysis", "Liftoff"],
+    launchOutput: function(n){
+         if (n%2===0 && n%3 ===0){
+            return 'LaunchCode!';
+        }
+        if (n%2===0){
+            return 'Launch!';
+        }
+        if (n%3===0){
+            return 'Code!';
+        }
+        if (n%5===0){
+            return 'Rocks!';
+        }
+    }
+};
+
+// function launchOutput(n){}
 
 module.exports = launchcode;
 
