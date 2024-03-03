1c5b7689ca56a7e039139219fd6a1afea647a9a6
kelleighm <kelleighmmartin@gmail.com>
2024-03-03 12:36:59 -0600

    Functions Exercise

--- functions/try-it/Shapes.js
+++ functions/try-it/Shapes.js
@@ -6,7 +6,7 @@
 
 function makeLine(size) {
   let line = '';
-  for (i=0; i < size; i++) {
+  for (let i=0; i < size; i++) {
     line += '#';
   }
   return line;
@@ -14,6 +14,70 @@ function makeLine(size) {
 
 console.log(makeLine(5)); 
 
+console.log("__________");
+
 function makeSquare(size) {
   let line = '';
-  for (let i = 0; i < size; i++)
+  for (let i = 0; i < size; i++) {
+  let row = '';
+    for ( let j=0; j < size; j++) {
+      row += '#'
+    }
+    console.log(row+'');
+  }
+}
+
+makeSquare(4) 
+
+console.log("__________");
+
+function makeRectangle(width, height) {
+  let rectangle = '';
+  for (let i = 0; i < height; i++) {
+    rectangle += (makeLine(width) + '\n');
+  }
+  return rectangle.slice(0, -1);
+}
+
+console.log(makeRectangle(4,2));
+
+console.log("__________");
+
+function makeDownwardStairs(height) {
+  let stairs = '';
+  for (let i = 0; i < height; i++) {
+    stairs += (makeLine(i+1) + '\n');
+  }
+  return stairs.slice(0, -1);
+}
+
+console.log(makeDownwardStairs(10)); 
+
+console.log("__________");
+
+
+function makeSpaceLine(numSpaces, numChars) {
+   spaces = '';
+  for (let i = 0; i < numSpaces; i++) {
+    spaces += " "
+  }
+  for (let j = 0; j < numChars; j++) {
+    spaces += '#'
+  }
+  for (let i = 0; i < numSpaces; i++) {
+    spaces += " "
+  }
+  return spaces
+}
+
+console.log(makeSpaceLine(3,5)); 
+
+function makeIsoscelesTriangle(height) {
+  let triangle = '';
+  for (let i = 0; i < height; i++) {
+    triangle += (makeSpaceLine(height - i - 1, 2*i + 1) + '\n');
+  }
+  return triangle.slice(0, -1);
+}
+
+console.log(makeIsoscelesTriangle(10)); 
